d52e7bf3acb193a6519e0ccbba693a0fdbb6f999
Marina Sundström <marna.li@outlook.com>
2023-01-08 01:55:37 +0100

    Update Unit Tests

--- BookScraper.Tests/UrlHelpersTest.cs
+++ BookScraper.Tests/UrlHelpersTest.cs
@@ -1,6 +1,6 @@
 ﻿namespace BookScraper.Tests;
 
-public class UnitTest1
+public class UrlHelpersTest
 {
     string baseUrl = "http://books.toscrape.com";
     string currentUrl = "http://books.toscrape.com/test";
@@ -9,23 +9,34 @@ public class UnitTest1
     public void Test1()
     {
         var newurl = UrlHelpers.AsAbsoluteUrl("http://books.toscrape.com", "http://books.toscrape.com/test/index.html", "../index.html");
+
+        Assert.Equal("http://books.toscrape.com/index.html", newurl);
+
     }
 
     [Fact]
     public void Test2()
     {
         var newurl = UrlHelpers.AsAbsoluteUrl("http://books.toscrape.com", "http://books.toscrape.com/test/index.html", "index.html");
+
+        Assert.Equal("http://books.toscrape.com/test/index.html", newurl);
+
     }
 
     [Fact]
     public void Test3()
     {
         var newurl = UrlHelpers.AsAbsoluteUrl("http://books.toscrape.com", "http://books.toscrape.com/test/test2/index.html", "index.html");
+
+        Assert.Equal("http://books.toscrape.com/test/test2/index.html", newurl);
     }
 
     [Fact]
     public void Test4()
     {
         var newurl = UrlHelpers.AsAbsoluteUrl("http://books.toscrape.com", "http://books.toscrape.com/test/test2/index.html", "../index.html");
-    }  
+
+        Assert.Equal("http://books.toscrape.com/test/index.html", newurl);
+
+    }
 }
